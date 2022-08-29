5740535740453afcee996217fc9a9347100260ec
Renata Santos <renatados.santosp@gmail.com>
2022-08-29 20:41:03 -0300

    determine winner method

--- GameRockPaperScissors/force-app/main/default/lwc/rockPaperScissors/rockPaperScissors.js
+++ GameRockPaperScissors/force-app/main/default/lwc/rockPaperScissors/rockPaperScissors.js
@@ -19,10 +19,37 @@ export default class RockPaperScissors extends LightningElement {
      */
     handleClick(event){
         this.playerGuess = event.target.name;
+        this.generateComputerGuess();
+        this.determineWinner();
     }
 
     generateComputerGuess(){
         const random = Math.floor(Math.random() * 3);
         console.log('the random number is ${random}');
+
+        if(random === 0){
+            this.computerGuess = ROCK;
+        }else if(random === 1){
+            this.computerGuess = PAPER;
+        }else{
+            this.computerGuess = SCISSORS;
+        }
+    }
+
+    determineWinner(){
+        if(this.playerGuess === this.computerGuess){
+            this.winner = 'draw!';
+            return;
+        }
+
+        if(this.playerGuess === ROCK && this.computerGuess === SCISSORS){
+            this.winner = 'you! :)';
+        }else if(this.playerGuess === SCISSORS && this.computerGuess === PAPER){
+            this.winner = 'you! :)';
+        }else if(this.playerGuess === PAPER && this.computerGuess === ROCK){
+            this.winner = 'you! :)';
+        }else{
+            this.winner = 'computer! :('
+        }
     }
 }
