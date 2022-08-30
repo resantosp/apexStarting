bdf4514c6945d6b2ca9435dd6d8ab9047f53272e
Renata Santos <renatados.santosp@gmail.com>
2022-08-30 08:47:15 -0300

    trying to adjust the padding

--- GameRockPaperScissors/force-app/main/default/lwc/rockPaperScissors/rockPaperScissors.js
+++ GameRockPaperScissors/force-app/main/default/lwc/rockPaperScissors/rockPaperScissors.js
@@ -1,11 +1,15 @@
 
 import { LightningElement } from 'lwc';
+import MYIMG from '@salesforce/resourceUrl/MyImg';
 
 const PAPER = 'paper';
 const ROCK = 'rock';
 const SCISSORS = 'scissors';
+const PLAYER_WIN = 'YOU WIN! :)';
 
 export default class RockPaperScissors extends LightningElement {
+    testeImagem = MYIMG;
+    
     playerScore = 0;
     computerScore = 0;
 
@@ -38,18 +42,23 @@ export default class RockPaperScissors extends LightningElement {
 
     determineWinner(){
         if(this.playerGuess === this.computerGuess){
-            this.winner = 'draw!';
+            this.winner = 'it is a draw! :|';
             return;
         }
 
         if(this.playerGuess === ROCK && this.computerGuess === SCISSORS){
-            this.winner = 'you! :)';
+            this.winner = PLAYER_WIN;
+            this.playerScore += 1;
         }else if(this.playerGuess === SCISSORS && this.computerGuess === PAPER){
-            this.winner = 'you! :)';
+            this.winner = PLAYER_WIN;
+            this.playerScore += 1;
         }else if(this.playerGuess === PAPER && this.computerGuess === ROCK){
-            this.winner = 'you! :)';
+            this.winner = PLAYER_WIN;
+            this.playerScore += 1;
         }else{
-            this.winner = 'computer! :('
+            this.winner = 'computer win! :(';
+            this.computerScore += 1;
         }
     }
+
 }
